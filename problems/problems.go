4b70bb627517f31919b0c85c9ac522e551e6f14e
jtpeller <70555819+jtpeller@users.noreply.github.com>
2025-02-09 18:38:34 -0700

    Update problems.go
    Add dates, fix warning.

--- problems/problems.go
+++ problems/problems.go
@@ -19,6 +19,7 @@ import (
 /**
  * P1() - Multiples of 3 or 5
  *  Find the sum of all the multiples of 3 or 5 below 1000.
+ *	Date	2021.01.20
  */
 func P1() int64 {
 	m := make([]int64, 0)
@@ -36,6 +37,7 @@ func P1() int64 {
  *  By considering the terms in the Fibonacci sequence whose values 
  * 		do not exceed four million, find the sum of the even-valued 
  *		terms.
+ *	Date	2021.01.20
  */
 func P2() int64 {
 	F := utils.Fibonacci(35)
@@ -51,6 +53,7 @@ func P2() int64 {
 /**
  * P3() - Largest Prime Factor
  *  What is the largest prime factor of the number 600851475143?
+ *	Date	2021.01.21
  */
 func P3() int64 {
 	fact := utils.PrimeFactorization(600851475143)
@@ -62,6 +65,7 @@ func P3() int64 {
  * P4() - Largest Palindrome Product
  *  Find the largest palindrome made from the product of two 3-digit 
  *		numbers.
+ *	Date	2021.01.21
  */
 func P4() int64 {
 	min := int64(100)
@@ -88,6 +92,7 @@ func P4() int64 {
  * P5() - Smallest Multiple
  *  What is the smallest positive number that is evenly divisible 
  *		by all of the numbers from 1 to 20?
+ *	Date	2021.01.21
  */
 func P5() int64 {
 	max := int64(20)
@@ -110,6 +115,7 @@ func P5() int64 {
  * P6() - Sum Square Difference
  *  Find the difference between the sum of the squares of the first 
  * 		one hundred natural numbers and the square of the sum.
+ *	Date	2021.01.23
  */
 func P6() int64 {
 	n := int64(100)
@@ -121,6 +127,7 @@ func P6() int64 {
 /**
  * P7() - 10,001st Prime
  * What is the 10,001st prime number?
+ *	Date	2021.01.23
  */
 func P7() int64 {
 	primes := utils.Primes(10001)
@@ -131,6 +138,7 @@ func P7() int64 {
  * P8() - Largest Product in a Series
  *  Find the thirteen adjacent digits in the 1000-digit number that 
  *		have the greatest product. What is the value of this product?
+ *	Date	2021.01.24
  */
 func P8() int64 {
 	p8 := "7316717653133062491922511967442657474235534919493496983520312774506326239578318016984801869478851843858615607891129494954595017379583319528532088055111254069874715852386305071569329096329522744304355766896648950445244523161731856403098711121722383113622298934233803081353362766142828064444866452387493035890729629049156044077239071381051585930796086670172427121883998797908792274921901699720888093776657273330010533678812202354218097512545405947522435258490771167055601360483958644670632441572215539753697817977846174064955149290862569321978468622482839722413756570560574902614079729686524145351004748216637048440319989000889524345065854122758866688116427171479924442928230863465674813919123162824586178664583591245665294765456828489128831426076900422421902267105562632111110937054421750694165896040807198403850962455444362981230987879927244284909188845801561660979191338754992005240636899125607176060588611646710940507754100225698315520005593572972571636269561882670428252483600823257530420752963450"
@@ -153,6 +161,7 @@ func P8() int64 {
  * P9() - Special Pythagorean Triplet
  *  There exists exactly one Pythagorean triplet for which 
  *		a + b + c = 1000. Find the product abc.
+ *	Date	2021.01.24
  */
 func P9() int64 {
 	triplet := func(sum int64) (int64, int64, int64) {
@@ -179,6 +188,7 @@ func P9() int64 {
 /**
  * P10() - Summation of Primes
  *  Find the sum of all the primes below two million.
+ *	Date	2021.01.24
  */
 func P10() int64 {
 	return utils.Sum(utils.PrimesUpTo(2e6))
@@ -189,6 +199,7 @@ func P10() int64 {
  * 	What is the greatest product of four adjacent numbers in the 
  *		same direction (up, down, left, right, or diagonally) in the
  *		20×20 grid?
+ *	Date	2025.01.28
  */
 func P11() int64 {
 	// raw is pulled from the site, and modified for storage
@@ -300,11 +311,12 @@ func P11() int64 {
  * P12() - Highly Divisible Triangular Number.
  *  What is the value of the first triangle number to have over five
  *		hundred divisors?
+ *	Date	2025.01.28
  */
 func P12() int64 {
 	// init
 	tnum := int64(0)
-	div := make([]int64, 0)
+	var div []int64
 
 	// begin the loop
 	idx := int64(0)
@@ -330,6 +342,7 @@ func P12() int64 {
  *	P13() - Large Sum
  * 	 Work out the first ten digits of the sum of the following 
  *		one-hundred 50-digit numbers.
+ *	Date	2025.01.28
  */
 func P13() int64 {
 	// take values
@@ -359,6 +372,7 @@ func P13() int64 {
  * P14() - Longest Collatz Sequence
  * 	Which starting number, under one million, produces the longest 
  *  	chain?
+ *	Date	2025.02.01
  */
 func P14() int64 {
 	// logic is: loop from one million down to zero
@@ -392,6 +406,7 @@ func P14() int64 {
 /**
  * P15() - Lattice Paths
  * 	How many routes through a 20x20 grid?
+ *	Date	2025.02.01
  */
 func P15() int64 {
 	return utils.C(40,20)
@@ -400,6 +415,7 @@ func P15() int64 {
 /**
  * P16() - Power Digit Sum
  *  What is the sum of the digits of 2^1000?
+ *	Date	2025.02.01
  */
 func P16() int64 {
 	num := pow(inew(2), inew(1000))			// compute 2^1000
@@ -412,6 +428,7 @@ func P16() int64 {
  * P17() - Number Letter Counts
  *  If all the numbers from 1 to 1000 (one thousand) inclusive were 
  * 		written out in words, how many letters would be used?
+ *	Date	2025.02.01
  */
 func P17() int64 {
 	// generate list using my num2words package
